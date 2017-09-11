c0002d8b3a13bdfa3812cceffae5bd7e91cd44e2
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2017-09-11 12:41:47 +0300

    Add some demo products

--- resources/database.sql
+++ resources/database.sql
@@ -26,7 +26,10 @@ VALUES ('Toys','All sorts of toys for children and childish adults.'),
        ('Doomsday devices', 'Sometimes you just want to watch the world burn and we have just the items for you!');
 
 INSERT INTO product (name, description, price)
-VALUES ('Illudium Q-36 Explosive Space Modulator', 'Pesky planets obstructing your view? Just blow them up with this handy space modulator!', 95990.50);
+VALUES ('Illudium Q-36 Explosive Space Modulator', 'Pesky planets obstructing your view? Just blow them up with this handy space modulator!', 95990.50),
+       ('Log! from Blammo', 'It''s log, it''s log, it''s big, it''s heavy, it''s wood.', 19.95),
+       ('Don''t whizz on the electric fence!', 'Fun board game for the whole family.', 59.50),
+       ('Fine leather jacket', 'I''m selling these fine leather jackets.', 500.0);
 
 INSERT INTO product_category (product_id, category_id)
-VALUES (1, 3);
+VALUES (1, 3), (2, 1), (3, 1), (4, 2);
